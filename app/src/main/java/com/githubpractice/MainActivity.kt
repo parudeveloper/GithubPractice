a9ef9efd3630a79f98c03eb6d3a356fea76e5776
parudeveloper <paramesh.developer@gmail.com>
2024-09-27 15:20:34 +0530

    Test Branch changes 2

--- app/src/main/java/com/githubpractice/MainActivity.kt
+++ app/src/main/java/com/githubpractice/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         // Git Comments
         // Demo
         // Demo 2
+        // Demo 4
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
